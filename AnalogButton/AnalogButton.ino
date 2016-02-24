db665a7166bbe32d6effd5fb99b6105292f76860
SDAMcIntyre <sdamcintyre+git@gmail.com>
2016-02-24 17:32:05 +1100

    now prints continuous serial read-out as well as when buttons are pressed

--- AnalogButton/AnalogButton.ino
+++ AnalogButton/AnalogButton.ino
@@ -1,9 +1,10 @@
 
-const int maxButton = 3; // 2 if you just want response buttons; 3 if you want "go" button
+int ledPin = 3;     
+int buttonPin = 0;   // analog pin for buttons
+const int nButtons = 3; 
+const int buttonReadValues[3] = {767,512,256}; // expected analog read values for each button
 const int analogErrorWindow = 50; // error window for reading analog input (buttons)
-int ledPin = 3;      // LED connected to digital pin 9
-int buttonPin = 0;   // switch circuit input connected to analog pin 3
-String pythonSays; // for reading commands from pythonboolean go = false;
+int buttonValue = 0;
 
 void setup(){
  //pin for driving LED
@@ -19,47 +20,16 @@ void setup(){
 
 
 void loop() {
-
-  while (Serial.available() <= 0) {
-    // wait for serial input
+  buttonValue = analogRead(buttonPin);
+  Serial.println(buttonValue);
+  for (int b=0; b<nButtons; b++) {
+    if ( buttonValue >= (buttonReadValues[b]-analogErrorWindow) and buttonValue <= (buttonReadValues[b]+analogErrorWindow) ) {
+      Serial.print("button pressed: ");
+      Serial.println(b);
+      digitalWrite(ledPin, HIGH);
+    }
   }
-  //get instructions from python over serial connection
-  pythonSays = Serial.readString();
-
-  //checking serial connection
-  if (pythonSays == "ping") {
-    Serial.println("ack");
-
-  // get responses
-  } else if (pythonSays == "go") {
-
-    // get the response
-    get_response();
- 
-  // other
-  } else {
-    Serial.print("I don't understand: ");
-    Serial.println(pythonSays);
-  }
+  delay(200);
+  digitalWrite(ledPin, LOW);
 }
 
-void get_response() {
-  int buttonValue = 0;
-  int button = -1;
-   
-  while (button <= 0 or button > maxButton) {
-    buttonValue = analogRead(buttonPin);
-    if( buttonValue >= (767-analogErrorWindow) and buttonValue <= (767+analogErrorWindow) ) { 
-      button = 1;
-      
-    } else if ( buttonValue >= (512-analogErrorWindow) and buttonValue <= (512+analogErrorWindow) ) { 
-     button = 2;
-     
-    } else if ( buttonValue >= (256-analogErrorWindow) and buttonValue <= (256+analogErrorWindow) ) { 
-     button = 3;
-    } else
-     button = 0;  // no button found to have been pushed 
-  }
-  Serial.println("response");
-  Serial.println(button);
-}
