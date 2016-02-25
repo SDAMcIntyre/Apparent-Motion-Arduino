70b70a44365713969db87b469e927d9183fce670
SDAMcIntyre <sdamcintyre+git@gmail.com>
2016-02-25 23:08:32 +1100

    implemented practice trials at the beginning

--- Experiment_AM-temporal-threshold.py
+++ Experiment_AM-temporal-threshold.py
@@ -18,13 +18,13 @@ except:        # default values
                 '06. First ISOI (ms)':200,
                 '07. Probe activation duration':5,
                 '08. Number of adaptive trials':40, 
-                '09. Pre-adaptive ISOIs':[], 
+                '09. Practice ISOIs':'500,300', 
                 '10. Use GO button':True,
                 '11. Folder for saving data':'test', 
                 '12. Device orientation (0 or 1)':0, 
                 '13. Arduino serial port':'/dev/cu.usbmodem1411', 
                 '14. Print arduino messages':False}
-exptInfo['15. Date and time']= data.getDateStr(format='%Y-%m-%d-%H-%M') #add the current time
+exptInfo['15. Date and time']= data.getDateStr(format='%Y-%m-%d_%H-%M-%S') #add the current time
 
 dlg = gui.DlgFromDict(exptInfo, title='Experiment details', fixed=['15. Date and time'])
 if dlg.OK:
@@ -33,20 +33,28 @@ else:
     core.quit() # the user hit cancel so exit
 
 stimToUse = [int(i) for i in exptInfo['03. Probes to use (1-4)'].split(',')]
+try:
+    preISOI = [int(i) for i in exptInfo['09. Practice ISOIs'].split(',')]
+except:
+    preISOI = []
 ## ----
 
 ## -- make folder/files to save data --
-dataFolder = './'+exptInfo['11. Folder for saving data']+'/'
-if not os.path.exists(dataFolder):
-    os.makedirs(dataFolder)
-    writeHeaders = True
+if exptInfo['08. Number of adaptive trials'] > 0:
+    dataFolder = './'+exptInfo['11. Folder for saving data']+'/'
+    if not os.path.exists(dataFolder):
+        os.makedirs(dataFolder)
+        writeHeaders = True
+    else:
+        writeHeaders = False
+    thresholdData = open(dataFolder + 'thresholdData.csv', 'a')
+    if writeHeaders: thresholdData.write('threshold,sd,order,participant,nProbes,separation,site,dateTime\n')
+    fileName = dataFolder + exptInfo['15. Date and time']+'_'+ exptInfo['01. Participant Code']
+    trialData = open(fileName+'.csv', 'w') 
+    trialData.write('ISOI,direction,correct\n')
 else:
-    writeHeaders = False
-thresholdData = open(dataFolder + 'thresholdData.csv', 'a')
-if writeHeaders: thresholdData.write('threshold,sd,order,participant,nProbes,separation,site,dateTime\n')
-fileName = dataFolder + exptInfo['15. Date and time']+'_'+ exptInfo['01. Participant Code']
-trialData = open(fileName+'.csv', 'w') 
-trialData.write('ISOI,direction,correct\n')
+    exptInfo['02. Test number'] -= 1 #practice only so don't increment the test number
+    toFile('lastParams.pickle', exptInfo) 
 ## ----
 
 ## -- setup staircase --
@@ -81,21 +89,22 @@ while not goButtonSet:
 ## ----
 
 ## -- run the experiment --
-trialNum = 0
-for suggestedISOI in s:
-    trialNum+=1
-    
+nPracTrials = len(preISOI)
+nTrials = nPracTrials + exptInfo['08. Number of adaptive trials']
+for trialNum in range(nTrials):
     ## turn off go button after first trial
-    if trialNum == 2:
+    if trialNum == 1:
         message = 'go button off'
         arduinoSays = ''
         while not arduinoSays == message:
             arduino.write(message)
             arduinoSays = arduino.readline().strip()
             if exptInfo['14. Print arduino messages'] and len(arduinoSays)> 0: print('arduino: {}' .format(arduinoSays))
-            
-        
-    isoi = int(round(suggestedISOI))
+    
+    if trialNum < nPracTrials:
+        isoi = preISOI[trialNum]
+    else:
+        isoi = int(round(s.next()))
     print('ISOI: {}ms' .format(isoi))
     direction = random.choice([0,1])
     
@@ -107,30 +116,37 @@ for suggestedISOI in s:
     
     correct = response == direction
     print('Correct : {}' .format(correct))
-    s.addResponse(correct, isoi)
-    trialData.write('{},{},{}\n' .format(isoi, direction, correct))
-    print '{} of {} trials complete\n\n' .format(trialNum, exptInfo['08. Number of adaptive trials'])
+    if trialNum < nPracTrials:
+        print('practice')
+    else:
+        s.addResponse(correct, isoi)
+        trialData.write('{},{},{}\n' .format(isoi, direction, correct))
+    print('{} of {} trials complete\n' .format(trialNum+1, nTrials))
 ## ----
 
 ## -- use quest to estimate threshold --
-q = data.QuestHandler(log(exptInfo['06. First ISOI (ms)']), log(exptInfo['06. First ISOI (ms)']), 
-                        pThreshold = 0.82, nTrials = exptInfo['08. Number of adaptive trials'],
-                        grain=0.1, range=log(1000),
-                        minVal = 0, maxVal = log(1000))
-isois = [log(i) for i in s.intensities]
-q.importData(isois,s.data)
-threshold = exp(q.mean())
-tSD = exp(q.sd())
-print('threshold: {}' .format(threshold))
-print('sd: {}' .format(tSD))
+if exptInfo['08. Number of adaptive trials'] > 0:
+    s.saveAsPickle(fileName) #special python binary file to save all the info
+    q = data.QuestHandler(log(exptInfo['06. First ISOI (ms)']), log(exptInfo['06. First ISOI (ms)']), 
+                            pThreshold = 0.82, nTrials = exptInfo['08. Number of adaptive trials'],
+                            grain=0.1, range=log(1000),
+                            minVal = 0, maxVal = log(1000))
+    isois = [log(i) for i in s.intensities]
+    q.importData(isois,s.data)
+    threshold = exp(q.mean())
+    tSD = exp(q.sd())
+    print('threshold: {}' .format(threshold))
+    print('sd: {}' .format(tSD))
 
-## -- save data --
-trialData.close()
-s.saveAsPickle(fileName) #special python binary file to save all the info
-thresholdData.write('{},{},{},{},{},{},{},{}\n' .format(threshold,tSD,
-                    exptInfo['02. Test number'],
-                    exptInfo['01. Participant Code'],
-                    len(stimToUse),
-                    exptInfo['04. Probe separation (cm)'],
-                    exptInfo['05. Stimulation site'],
-                    exptInfo['15. Date and time']))
+    thresholdData.write('{},{},{},{},{},{},{},{}\n' .format(threshold,tSD,
+                        exptInfo['02. Test number'],
+                        exptInfo['01. Participant Code'],
+                        len(stimToUse),
+                        exptInfo['04. Probe separation (cm)'],
+                        exptInfo['05. Stimulation site'],
+                        exptInfo['15. Date and time']))
+    thresholdData.close()
+    trialData.close()
+else:
+    print('Practice only, no data saved.')
+    
